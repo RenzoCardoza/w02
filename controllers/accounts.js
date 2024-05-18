91e3643b0f1fe3abbf70ea1ac1157de5e5ffc722
Renzo Cardoza <darkarcid@hotmail.com>
2024-05-18 20:39:19 -0300

    changes to the api docs

--- controllers/accounts.js
+++ controllers/accounts.js
@@ -2,18 +2,6 @@ const mongodb = require('../data/database')
 const ObjectId = require('mongodb').ObjectId
 const bcrypt = require('bcrypt')
 
-const getAllAccounts = async (req, res) => {
-    //#swagger.tags = ['Accounts']
-    const result = await mongodb.getDatabase().db('LolBase').collection('accounts').find()
-    result.toArray().then((champions, err) => {
-        if (err) {
-            res.status(400).json({ message: err })
-        }
-        res.setHeader('Content-Type', 'application/json')
-        res.status(200).json(champions)
-    })
-}
-
 const createAccount = async (req, res) => {
     let account = {
         account_first_name: req.body.account_first_name,
@@ -29,4 +17,32 @@ const createAccount = async (req, res) => {
     }
 }
 
-module.exports = { createAccount, getAllAccounts }
+const updateAccount = async (req, res) => {
+    //#swagger.tags = ['Champions']
+    const accId = new ObjectId(req.params.id)
+    let account = {
+        account_first_name: req.body.account_first_name,
+        account_last_name: req.body.account_last_name,
+        account_password: bcrypt.hashSync(req.body.account_password, 10), 
+        account_email: req.body.account_email,
+    }
+    const response = await mongodb.getDatabase().db('LolBase').collection('accounts').replaceOne({ _id: accId }, account)
+    if (response.modifiedCount > 0){
+        res.status(204).send()
+    } else {
+        res.status(500).json(response.error || 'Something went wrong with updating the account')
+    }
+}
+
+const deleteAccount = async (req, res) => {
+    //#swagger.tags = ['Champions']
+    const accId = new ObjectId(req.params.id)
+    const response = await mongodb.getDatabase().db('LolBase').collection('accounts').deleteOne({ _id: accId })
+    if (response.deletedCount > 0){
+        res.status(204).send()
+    } else {
+        res.status(500).json(response.error || 'Something went wrong while deleting the account')
+    }
+}
+
+module.exports = { createAccount, updateAccount, deleteAccount }
--- routes/accounts.js
+++ routes/accounts.js
@@ -3,14 +3,28 @@ const router = express.Router()
 const accountController = require('../controllers/accounts')
 const validator = require('../utilities/accountValidator')
 
-// ROUTES //  
-router.get('/', accountController.getAllAccounts)
+const { isAuthenticated } = require('../middleware/authenticate')
 
+// ROUTES //  
 router.post(
     '/', 
     validator.accountValidation,
     validator.validationHandler,
     accountController.createAccount
+)
+
+router.put(
+    '/:id',
+    isAuthenticated,
+    validator.accountValidation,
+    validator.validationHandler,
+    accountController.updateAccount
+)
+
+router.delete(
+    '/:id',
+    isAuthenticated,
+    accountController.deleteAccount
 )
 
 module.exports = router
