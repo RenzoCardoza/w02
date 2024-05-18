708c2e7d92e2d8616b07bd82ea565504257c5046
Renzo Cardoza <darkarcid@hotmail.com>
2024-05-18 20:48:42 -0300

    logout

--- routes/index.js
+++ routes/index.js
@@ -10,7 +10,7 @@ router.use('/account', require('./accounts'))
 
 router.get('/login', passport.authenticate('github'), (req, res) => {})
 
-router.get('/login', function(req, res, next) {
+router.get('/logout', function(req, res, next) {
     req.logout(function(err){ 
         if (err) { return next(err) }
         res.redirect('/')
